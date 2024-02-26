1eb3d087c2a604251167d92c7b7c1bed48c04652
junesdream <norisori@gmail.com>
2024-02-26 18:10:20 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Accordian from "./components/accordion";
+
+function App() {
+	return <div className="App">
+    {/* Accordian component */}
+    <Accordian />
+  </div>;
+}
+
+export default App;
---
+++ src/components/accordion/data.js
@@ -0,0 +1,24 @@
+const data = [
+	{
+		id: "1",
+		question: "What are the characteristics of spring?",
+		answer: "Spring is characterized by warmer temperatures, blooming flowers, budding trees, and increased daylight hours. It marks the transition from winter to summer and is often associated with renewal, growth, and rejuvenation.",
+	},
+	{
+		id: "2",
+		question: "What outdoor activities can I enjoy during spring?",
+		answer: "During spring, you can enjoy various outdoor activities such as hiking, picnicking, gardening, cycling, and birdwatching. It's a great time to explore nature and engage in recreational pursuits.",
+	},
+	{
+		id: "3",
+		question: "How does spring affect wildlife?",
+		answer: "Spring has a significant impact on wildlife. Many animals emerge from hibernation, migratory birds return from their winter habitats, and various species undergo mating and nesting behaviors. It's a vital time for biodiversity and ecosystem dynamics.",
+	},
+	{
+		id: "4",
+		question: "How can I track my order?",
+		answer: "To track your order, you can log in to your account and check the status of your order on the order page. Alternatively, you can also contact our customer service for further assistance.",
+	},
+];
+
+export default data;
